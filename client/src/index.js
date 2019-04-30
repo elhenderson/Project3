67f28d5030fc6301b3cc1785ffa0ad705e23e56a
elhenderson <evann.henderson2@yahoo.com>
2019-04-29 21:06:54 -0500

    fir push

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
