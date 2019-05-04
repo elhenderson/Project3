b29f4f485af75a2a8a2f2920d9012e7c10ce67e9
elhenderson <evann.henderson2@yahoo.com>
2019-05-04 13:37:32 -0500

    post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,17 @@ router.get("/getPosts", (req, res) => {
     .then((result) => res.json(result));
 })
 
+router.post("/post", (req, res) => {
+    console.log(req.body);
+    Post.create(req.body)
+    .then(() => res.json({success:true}))
+    .catch(err => console.log(err))
+})
 
-router.post()
+// router.delete("/post/:id", (req, res) => {
+//     Post.
+// })
+
+
 
 module.exports = router;
