648ccfe9fe83550e57b83e112110504f3c8a4e00
elhenderson <evann.henderson2@yahoo.com>
2019-05-04 13:58:05 -0500

    apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,24 +3,80 @@ const sequelize =require('sequelize');
 const router = express.Router();
 const models = require('../models');
 const Post = models.Post
-const User = require("../models/user");
+const User = models.User;
 
+
+//Post routes
 router.get("/getPosts", (req, res) => {
     Post.findAll()
     .then((result) => res.json(result));
 })
 
 router.post("/post", (req, res) => {
-    console.log(req.body);
     Post.create(req.body)
     .then(() => res.json({success:true}))
     .catch(err => console.log(err))
 })
 
-// router.delete("/post/:id", (req, res) => {
-//     Post.
-// })
+router.delete("/deletePost/:id", (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(() => res.json({success: true}))
+    .catch(err => console.log(err)) 
+})
 
+router.put("/editPost/:id", (req, res) => {
+    Post.update(
+        req.body,
+        {where : {
+            id: req.params.id
+        }}
+    )
+    .then(() => res.json({success: true}))
+    .catch(err => console.log(err))
+})
+
+
+//User routes
+router.get("/user/:id", (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(() => res.json({success: true}))
+    .catch(err => console.log(err))
+})
+
+router.post("/addUser", (req, res) => {
+    User.create(req.body)
+    .then(() => res.json({success: true}))
+    .catch(err => console.log(err))
+})
+
+router.put("/editUser/:id", (req, res) => {
+    User.update(
+        req.body,
+        {where : {
+            id: req.params.id
+        }}
+    )
+    .then(() => res.json({success: true}))
+    .catch(err => console.log(err))
+})
+
+router.delete("/deleteUser/:id", (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(() => res.json({success: true}))
+    .catch(err => console.log(err)) 
+})
 
 
 module.exports = router;
