27c273110e84436eeda559080c134cf333939f1c
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-05-11 11:50:51 -0500

    styling page

--- client/src/containers/Userpage.js
+++ client/src/containers/Userpage.js
@@ -5,7 +5,8 @@ import React, {Component} from 'react';
 export default class Secret extends Component{
     render(){
         return(
-            <div>You can View this when you log in</div>
+            <div>You can View this when you log in
+            </div>
         )
     }
 }
