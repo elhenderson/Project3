fd1c659ffcd59d69a90cd3d3fdfaf60bd0bd8d85
mango <elig7393@gmail.com>
2019-05-15 21:33:50 -0500

    added recollect widget to About page

--- client/src/containers/About.js
+++ client/src/containers/About.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
-function About() {
-  return (
-    <div className="">
-        ABOUT PAGE
-    </div>
-  );
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this._ref = React.createRef();
+}
+  componentDidMount() {
+    var rcw = document.querySelector("#rCw");
+    document.querySelector("#recollect").appendChild(rcw);
+    rcw.setAttribute('style', 'display:block');
+}
+
+    render() {
+    return (
+      <div className="">
+          ABOUT PAGE
+        <div id="recollect"></div>
+      </div>
+    );
+  }
 }
 
 export default About;
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -5,5 +5,6 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 // import 'mdbreact/dist/css/mdb.css';
 
+document.querySelector("#rCw").setAttribute('style', 'display:none');
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
