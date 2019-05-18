976e744d7fef4e504a7f3bcef8b6587dda3cad0d
mango <elig7393@gmail.com>
2019-05-18 10:15:43 -0500

    About page updated with recollect widget

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 import postReducer from './postReducer';
 import userReducer from './userReducer';
-
+document.querySelector("#rCw").setAttribute('style', 'display:none');
 const rootReducer = combineReducers({
     post: postReducer
 })
