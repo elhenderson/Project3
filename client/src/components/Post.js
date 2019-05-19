6d041c193262c6dc1562190ed9c59986b9752bf6
hliang <hliang@github>
2019-05-19 09:51:07 -0500

    card style posts

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -3,6 +3,7 @@ import Proptypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../actions/postActions'
 import JwPagination from 'jw-react-pagination'
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdbreact';
 
 
 class Post extends Component {
@@ -31,22 +32,32 @@ class Post extends Component {
     
     const postsList = this.state.pageOfItems.map((post, index) => {
       return (
-        <div key={post.id}>
-          <br />
-          <a href={"/explore/" + post.id}><h2>{post.title}</h2></a>
-          <p>{post.location}</p>
-          <img src={`${post.photo[0]}`} width="250" height="250" />        
-        </div>
+        // <div key={index}>
+        /* <MDBCol className="col-md-6"> */
+          <MDBCard key={index} style={{ width: "22rem", maxWidth: "95%" }} className="mb-2 mb-4 mr-4 p-2 col-lg-4 col-md-3 col-sm-12">
+            <MDBCardBody>
+              <MDBCardTitle>
+                <a href={"/explore/" + post.id}> {post.title} </a>
+              </MDBCardTitle>
+              <MDBCardText> {post.location} </MDBCardText>
+            </MDBCardBody>
+            <MDBCardImage className="img-fluid" src={post.photo[0]} waves />
+          </MDBCard>
+        /* </MDBCol> */
+        // </div>
       )
 
     })
     return (
-      <div>
-        <h1>Explore</h1>
-        {postsList}
+      <MDBContainer>
+        <h2 className="my-4 text-center font-weight-bold" style={{ color: "#0097a7" }}>Explore</h2>
+        <MDBContainer className="d-flex flex-wrap bd-highlight example-parent">
+          {postsList}
+
+        </MDBContainer>
         {this.props.postArray.map(item => <div key={item.id}>{item.name}</div>)}
         <JwPagination items={postArray} onChangePage={this.onChangePage}/>
-      </div>
+      </MDBContainer>
     )
   }
 }
