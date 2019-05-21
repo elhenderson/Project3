187727261c522299a9e3bb912821a25177105b93
hliang <hliang@github>
2019-05-20 19:32:42 -0500

    center components in home page

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -4,17 +4,11 @@ import Jumbotron from "../components/Jumbotron"
 
 function Home() {
     return (
-        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", "margin": 0, "width": "100%", "textAlign": "center"}}>
+        <div className="container">
+        <div >
             <Jumbotron />
-            <div
-                style={{margin: 0, "width": "100%", "textAlign": "center"}}
-                className="container text-center">
-                 <br />
-                <div>
-                    <CarouselPage />
-
-                </div>
-            </div>
+            <CarouselPage />
+        </div>
         </div>
 
     );
